0b8690a12eb93bcb67d67a2339edb08dacb459a0
Mike Loomis <mikeloomisgg@gmail.com>
2019-04-26 06:30:12 -0500

    Part 5 Header/Implementation split

--- db.cpp
+++ db.cpp
@@ -7,6 +7,59 @@
 #include <cctype>
 #include <algorithm>
 
+Pager::Pager(const std::string &filename)
+    : file(filename, std::ios::in | std::ios::out | std::ios::app | std::ios::binary), file_length(), pages() {
+  file.close();
+  file.open(filename, std::ios::in | std::ios::out | std::ios::binary);
+  if (!file) {
+    std::cerr << "Unable to open file.\n";
+    exit(EXIT_FAILURE);
+  }
+  file.seekg(0, std::fstream::end);
+  file_length = file.tellg();
+  file.seekg(0, std::fstream::beg);
+}
+
+Page &Pager::get_page(std::size_t page_num) {
+  if (page_num > TABLE_MAX_PAGES) {
+    std::cerr << "Tried to fetch page number out of bounds. " << page_num << " > " << TABLE_MAX_PAGES << '\n';
+    exit(EXIT_FAILURE);
+  }
+
+  if (!pages[page_num].cached) {
+    std::size_t num_pages = file_length / PAGE_SIZE;
+
+    if (file_length % PAGE_SIZE) {
+      num_pages++;
+    }
+    if (page_num <= num_pages) {
+      file.seekg(page_num * PAGE_SIZE, std::fstream::beg);
+      file.read(pages[page_num].data.data(), PAGE_SIZE);
+      if (file.eof()) {
+        file.clear();
+      }
+    }
+    pages[page_num].cached = true;
+  }
+  return pages[page_num];
+}
+
+void Pager::flush(std::size_t page_num, std::size_t size) {
+  if (!pages[page_num].cached) {
+    std::cerr << "Tried to flush uncached page\n";
+    exit(EXIT_FAILURE);
+  }
+
+  file.seekg(page_num * PAGE_SIZE, std::fstream::beg);
+
+  file.write(pages[page_num].data.data(), size);
+  pages[page_num].cached = false;
+}
+
+Table::Table(const std::string &filename)
+    : pager(filename),
+      num_rows(pager.file_length / ROW_SIZE) {};
+
 void print_row(const Row &row) {
   std::cout << "(" << row.id << ", " << row.username.data() << ", " << row.email.data() << ")\n";
 }
--- db.hpp
+++ db.hpp
@@ -72,63 +72,18 @@ struct Pager {
   std::size_t file_length;
   std::array<Page, TABLE_MAX_PAGES> pages;
 
-  explicit Pager(const std::string &filename)
-      : file(filename, std::ios::in | std::ios::out | std::ios::app | std::ios::binary), file_length(), pages() {
-    file.close();
-    file.open(filename, std::ios::in | std::ios::out | std::ios::binary);
-    if (!file) {
-      std::cerr << "Unable to open file.\n";
-      exit(EXIT_FAILURE);
-    }
-    file.seekg(0, std::fstream::end);
-    file_length = file.tellg();
-    file.seekg(0, std::fstream::beg);
-  };
+  explicit Pager(const std::string &filename);
 
-  Page &get_page(std::size_t page_num) {
-    if (page_num > TABLE_MAX_PAGES) {
-      std::cerr << "Tried to fetch page number out of bounds. " << page_num << " > " << TABLE_MAX_PAGES << '\n';
-      exit(EXIT_FAILURE);
-    }
+  Page &get_page(std::size_t page_num);
 
-    if (!pages[page_num].cached) {
-      std::size_t num_pages = file_length / PAGE_SIZE;
-
-      if (file_length % PAGE_SIZE) {
-        num_pages++;
-      }
-      if (page_num <= num_pages) {
-        file.seekg(page_num * PAGE_SIZE, std::fstream::beg);
-        file.read(pages[page_num].data.data(), PAGE_SIZE);
-        if (file.eof()) {
-          file.clear();
-        }
-      }
-      pages[page_num].cached = true;
-    }
-    return pages[page_num];
-  }
-
-  void flush(std::size_t page_num, std::size_t size) {
-    if (!pages[page_num].cached) {
-      std::cerr << "Tried to flush uncached page\n";
-      exit(EXIT_FAILURE);
-    }
-
-    file.seekg(page_num * PAGE_SIZE, std::fstream::beg);
-
-    file.write(pages[page_num].data.data(), size);
-    pages[page_num].cached = false;
-  }
+  void flush(std::size_t page_num, std::size_t size);
 };
 
 struct Table {
   Pager pager;
   std::size_t num_rows;
 
-  explicit Table(const std::string &filename)
-      : pager(filename),
-        num_rows(pager.file_length / ROW_SIZE) {};
+  explicit Table(const std::string &filename);
 };
 
 void print_row(const Row &row);
